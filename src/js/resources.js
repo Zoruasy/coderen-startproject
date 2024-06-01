49fab3342a65c4d3338d78fe510a7e4c3789bcd3
Zoruasy <1076302@hr.nl>
2024-06-01 14:22:06 +0200

    new commit

--- src/js/resources.js
+++ src/js/resources.js
@@ -5,7 +5,9 @@ const dragonImage = new ImageSource('/images/dragon.png');
 const spaceshipImage = new ImageSource('/images/spaceship.png');
 const bulletImage = new ImageSource('/images/bullet.png');
 const laserImage = new ImageSource('/images/laser.png');
-const backgroundImage = new ImageSource('/images/background.png'); 
+const backgroundImage = new ImageSource('/images/background.png');
+const buttonImage = new ImageSource('/images/button.png');
+const gameLogoImage = new ImageSource('/images/gamelogo.png');
 
 // Group resources in an object
 const Resources = {
@@ -13,7 +15,9 @@ const Resources = {
     Spaceship: spaceshipImage,
     Bullet: bulletImage,
     Background: backgroundImage,
-    Laser: laserImage
+    Laser: laserImage,
+    Button: buttonImage,
+    GameLogo: gameLogoImage,
 };
 
 // Create a loader with all the resources
--- src/js/scenes/startgame.js
+++ src/js/scenes/startgame.js
@@ -1,38 +1,51 @@
-import { Scene, Label, Font, FontUnit, Color, Actor, vec } from "excalibur";
+import { Scene, Label, Font, FontUnit, Color, Actor, vec, Sprite } from "excalibur";
 import { Resources } from '../resources.js';
 
 export class StartGame extends Scene {
     onInitialize(engine) {
-        // Game Over text
+      // achtergrond
+        const background = new Actor({
+            pos: vec(engine.drawWidth / 2, engine.drawHeight / 2),
+            width: engine.drawWidth,
+            height: engine.drawHeight
+        });
+        background.graphics.use(Resources.Background.toSprite());
+        this.add(background);
+
+        // Game Logo
+        const gameLogo = new Actor({
+            pos: vec(engine.drawWidth / 2, engine.drawHeight / 2 - 250),
+            width: 200,
+            height: 100
+        });
+        gameLogo.graphics.use(Resources.GameLogo.toSprite());
+        gameLogo.anchor.setTo(0.5, 0.5);
+        this.add(gameLogo);
+
+        // Game Title
         const gameStartLabel = new Label({
-            text: 'Start gaming',
+           
             pos: vec(engine.drawWidth / 2, engine.drawHeight / 2 - 100),
-            font: new Font({
-                family: 'Arial',
-                size: 50,
-                unit: FontUnit.Px,
-                color: Color.Red,
-                textAlign: 'center'
-            })
+           
         });
         
         gameStartLabel.anchor.setTo(0.5, 0.5);
         this.add(gameStartLabel);
 
-        // Restart button
+        // Start Button
         const startButton = new Actor({
-            pos: vec(engine.drawWidth / 2, engine.drawHeight / 2),
+            pos: vec(engine.drawWidth / 2, engine.drawHeight / 2 + 50),
             width: 200,
-            height: 50,
-            color: Color.Gray
+            height: 50
         });
+        startButton.graphics.use(Resources.Button.toSprite());
         startButton.anchor.setTo(0.5, 0.5);
         this.add(startButton);
 
-        // Button text
+        // Button Text
         const buttonText = new Label({
-            text: 'Start',
-            pos: vec(engine.drawWidth / 2, engine.drawHeight / 2),
+            
+            pos: vec(engine.drawWidth / 2, engine.drawHeight / 2 + 50),
             font: new Font({
                 family: 'Arial',
                 size: 30,
@@ -41,10 +54,10 @@ export class StartGame extends Scene {
                 textAlign: 'center'
             })
         });
-        buttonText.anchor.setTo(0.5, .5);
+        buttonText.anchor.setTo(0.5, 0.5);
         this.add(buttonText);
 
-        // Click event to go to start scene
+        // Click event to go to main game scene
         startButton.on('pointerup', () => {
             engine.goToScene('maingame');
         });
@@ -52,5 +65,14 @@ export class StartGame extends Scene {
         // Ensure the button is interactive
         startButton.enableCapturePointer = true;
         startButton.pointer.useGraphicsBounds = true;
+
+        // Hover effect
+        startButton.on('pointerenter', () => {
+            startButton.color = Color.LightGray;
+        });
+
+        startButton.on('pointerleave', () => {
+            startButton.color = Color.Gray;
+        });
     }
 }
